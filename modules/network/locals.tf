b1be90fdd57d1b952345337e8ca8aac8f1cb07db
seunayolu <seunayolu@gmail.com>
2025-11-05 19:57:04 +0100

    update

--- modules/network/locals.tf
+++ modules/network/locals.tf
@@ -6,10 +6,10 @@ locals {
   azs = slice(data.aws_availability_zones.available.names, 0, 3)
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb"                                       = 1
+    "kubernetes.io/role/elb" = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb"                              = 1
+    "kubernetes.io/role/internal-elb" = 1
   }
 }
